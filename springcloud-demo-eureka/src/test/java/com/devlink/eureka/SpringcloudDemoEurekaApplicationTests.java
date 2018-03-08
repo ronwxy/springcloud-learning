f19678c7e3f811de6f06a723f532d4eab7e454a1
wuxy <wuxy@ieyecloud.com>
2018-03-08 14:26:20 +0800

    add config server

---
+++ springcloud-demo-eureka/src/test/java/com/devlink/eureka/SpringcloudDemoEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devlink.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudDemoEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
